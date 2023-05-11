3b7bc621d8f3d9d9842e88b59b5613417b81f55f
CapBlood <stalker.anonim@mail.ru>
2023-05-11 22:36:36 +0300

    refactor

--- src/lecture_automator/cli.py
+++ src/lecture_automator/cli.py
@@ -1,13 +1,6 @@
-import glob
-import os
-import tempfile
-
 import click
 
-from lecture_automator.gen_speech.gen_speech import texts_to_speeches
-from lecture_automator.gen_video.gen_video import generate_video
-from lecture_automator.marp_api import generate_marp_slides
-from lecture_automator.parser.parser import parse_md
+from lecture_automator.compiler import compile_text_md
 
 
 @click.command()
@@ -41,13 +34,10 @@ def convert_md_to_mp4(input_md, out_path, scale, vformat):
     with open(input_md) as file:
         md_text = file.read()
 
-    md_data = parse_md(md_text)
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        generate_marp_slides(tmpdirname, md_data['md_text'], scale=scale)
-        slide_images = glob.glob(os.path.join(tmpdirname, 'Slide.*'))
-        audio_paths = texts_to_speeches(md_data['speech'], tmpdirname)
-        generate_video(slide_images, audio_paths, out_path, vformat=vformat)
+    compile_text_md(
+        md_text, out_path=out_path,
+        vformat=vformat, scale=scale
+    )
 
 
 if __name__ == '__main__':
